7ca8ae22c6654d33f53d97ce277c1e5d32b8d5ad
Nikki Strømsnes <hello@tinybits.dk>
2022-03-28 20:53:08 +0200

    refactor: upgrade to v0.2.1 of the SDK

--- create-react-app/src/SingleFeatures.js
+++ create-react-app/src/SingleFeatures.js
@@ -1,28 +1,40 @@
-import { useHasFeature } from "@usebool/sdk-react";
+import { useHasFeature } from '@usebool/sdk-react';
 import { Badge } from './Badge';
 
 export const SingleFeatures = () => {
-    const featureOne = useHasFeature('E2E_ENABLED');
-    const featureTwo = useHasFeature('E2E_DISABLED');
-    const featureThree = useHasFeature('NONEXISTENT_FEATURE');
+  const featureOne = useHasFeature('E2E_ENABLED');
+  const featureTwo = useHasFeature('E2E_DISABLED');
+  const featureThree = useHasFeature('NONEXISTENT_FEATURE');
 
-    return (
-        <>
-            <tr>
-                <td>{featureOne.key}</td>
-                <td><Badge value={true}/></td>
-                <td><Badge value={featureOne.value}/></td>
-            </tr>
-            <tr>
-                <td>{featureTwo.key}</td>
-                <td><Badge value={false}/></td>
-                <td><Badge value={featureTwo.value}/></td>
-            </tr>
-            <tr>
-                <td>NONEXISTENT_FEATURE</td>
-                <td><Badge value={false}/></td>
-                <td><Badge value={featureThree.value}/></td>
-            </tr>
-        </>
-    );
-}
+  return (
+    <>
+      <tr>
+        <td>E2E_ENABLED</td>
+        <td>
+          <Badge value={true} />
+        </td>
+        <td>
+          <Badge value={featureOne} />
+        </td>
+      </tr>
+      <tr>
+        <td>E2E_DISABLED</td>
+        <td>
+          <Badge value={false} />
+        </td>
+        <td>
+          <Badge value={featureTwo} />
+        </td>
+      </tr>
+      <tr>
+        <td>NONEXISTENT_FEATURE</td>
+        <td>
+          <Badge value={false} />
+        </td>
+        <td>
+          <Badge value={featureThree} />
+        </td>
+      </tr>
+    </>
+  );
+};
